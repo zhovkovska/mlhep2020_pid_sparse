4942478ba5070351b9ed9bfc2df866dd526022a7
Valeriia <v.zhovkovskaya@gmail.com>
2020-07-28 16:17:32 +0200

    final_score 0.31

--- sparse_model.py
+++ sparse_model.py
@@ -9,14 +9,16 @@ class SparseNet(nn.Module):
     def __init__(self, input_dim, device, threshold=1.):
         super(SparseNet, self).__init__()
         self.fc1 = LinearSVDO(input_dim, 100, threshold=threshold, device=device)
-        self.fc2 = LinearSVDO(100, 100, threshold=threshold, device=device)
-        self.fc3 = LinearSVDO(100,  6, threshold=threshold, device=device)
+        self.fc2 = LinearSVDO(100, 50, threshold=threshold/5., device=device)
+        self.fc3 = LinearSVDO(50, 50, threshold=threshold/10., device=device)
+        self.fc4 = LinearSVDO(50,  6, threshold=threshold/50., device=device)
         # verify that your model have threshold _Parameter_!
         # and that requires_grad=False
         self.threshold = Parameter(torch.as_tensor(threshold), requires_grad=False)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.log_softmax(self.fc3(x), dim=1)
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
+        x = F.leaky_relu(self.fc3(x))
+        x = F.log_softmax(self.fc4(x), dim=1)
         return x
